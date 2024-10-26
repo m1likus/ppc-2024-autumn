f05284b0934067691c055b38e32ed76529f07ae4
m1likus <m1likus@yandex.ru>
2024-10-26 20:56:44 +0300

    tasks

--- tasks/seq/kabalova_v_count_symbols/perf_tests/main.cpp
+++ tasks/seq/kabalova_v_count_symbols/perf_tests/main.cpp
@@ -1,31 +1,34 @@
-// Copyright 2023 Nesterov Alexander
+// Copyright 2024 Kabalova Valeria
 #include <gtest/gtest.h>
 
+#include <cstring>
 #include <vector>
 
 #include "core/perf/include/perf.hpp"
-#include "seq/example/include/ops_seq.hpp"
+#include "seq/kabalova_v_count_symbols/include/count_symbols.hpp"
 
-TEST(sequential_example_perf_test, test_pipeline_run) {
-  const int count = 100;
-
+TEST(kabalova_v_count_symbols_seq_perf_test, test_pipeline_run) {
+  std::string string = "string";
+  std::string str;
+  for (int i = 0; i < 20000; i++) {
+    str += string;
+  }
   // Create data
-  std::vector<int> in(1, count);
   std::vector<int> out(1, 0);
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str.data()));
+  taskDataSeq->inputs_count.emplace_back(str.size());
   taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
   taskDataSeq->outputs_count.emplace_back(out.size());
 
   // Create Task
-  auto testTaskSequential = std::make_shared<nesterov_a_test_task_seq::TestTaskSequential>(taskDataSeq);
+  auto testTaskSequential = std::make_shared<kabalova_v_count_symbols_seq::TestTaskSequential>(taskDataSeq);
 
   // Create Perf attributes
   auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
+  perfAttr->num_running = 5000;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perfAttr->current_timer = [&] {
     auto current_time_point = std::chrono::high_resolution_clock::now();
@@ -40,29 +43,31 @@ TEST(sequential_example_perf_test, test_pipeline_run) {
   auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
   perfAnalyzer->pipeline_run(perfAttr, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_EQ(count, out[0]);
 }
 
-TEST(sequential_example_perf_test, test_task_run) {
-  const int count = 100;
+TEST(kabalova_v_count_symbols_seq_perf_test, test_task_run) {
+  std::string string = "string";
+  std::string str;
+  for (int i = 0; i < 20000; i++) {
+    str += string;
+  }
 
   // Create data
-  std::vector<int> in(1, count);
   std::vector<int> out(1, 0);
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str.data()));
+  taskDataSeq->inputs_count.emplace_back(str.size());
   taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
   taskDataSeq->outputs_count.emplace_back(out.size());
 
   // Create Task
-  auto testTaskSequential = std::make_shared<nesterov_a_test_task_seq::TestTaskSequential>(taskDataSeq);
+  auto testTaskSequential = std::make_shared<kabalova_v_count_symbols_seq::TestTaskSequential>(taskDataSeq);
 
   // Create Perf attributes
   auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
+  perfAttr->num_running = 5000;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perfAttr->current_timer = [&] {
     auto current_time_point = std::chrono::high_resolution_clock::now();
@@ -77,10 +82,4 @@ TEST(sequential_example_perf_test, test_task_run) {
   auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
   perfAnalyzer->task_run(perfAttr, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_EQ(count, out[0]);
-}
-
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
 }
