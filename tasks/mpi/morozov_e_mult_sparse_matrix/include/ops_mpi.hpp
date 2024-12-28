8ca766bfe1ee74e262ee738c5230642ab3f3060e
Egor1dzeN-learn <134601909+Egor1dzeN-learn@users.noreply.github.com>
2024-12-28 19:47:09 +0300

    Морозов Егор. Задача 3. Вариант 5. Умножение разреженных матриц. Элементы типа double.  Формат хранения матрицы – столбцовый (CCS). (#681)
    Формат хранения - **столбцовый**:
    
    ![image](https://github.com/user-attachments/assets/38a565df-a922-455d-b0bd-e1f29587e189)
    Для матрицы выше необходимо хранить 3 массива данных:
    **val**: [2,3,1,4] (ненулевые элементы)
    **row_indices**: [1,0,2,1] (индексы строк, в которой находится каждый
    элемент из val)
    **col_ptr**: [0,1,3,4] (индексы элементов, с которых начинается столбец)
    P.S. также необходимо хранить размеры матрицы в обычном виде, т.к. может
    быть столбец нулей, соответственно из col_ptr будет неизвестно узнать
    начальные размеры матрицы, чтобы позже проверить можно ли 2 матрицы
    умножать (кол-во столбцов первой == кол-ву строк второй)
    
    ! В тестах изначально используются матрицы в привычном виде (вектор
    векторов), а позже он преобразуется в формат CCS - это сделано
    исключительно для читабельности и проверки правильности работы тестов
    
    Последовательная версия:
    1. Преобразуем матрицы из CCS в привычный вид, при этом для удобства 2
    матрицу разворачиваем на 90 градусов (для удобства получим, что нужно
    будет просто умножить строку на строку)
    2. Перемножаем каждую строку на столбец, получаем матрицу - ответ
    
    Параллельная версия:
    1. Преобразуем матрицы из CCS в привычный вид, при этом для удобства 2
    матрицу разворачиваем на 90 градусов (для удобства получим, что нужно
    будет просто умножить строку на строку)
    2. Каждому процессу выдаем 2 вектора, которые ему нужно умножить и
    вернуть в 0 процесс для формирования матрицы - ответа. Также каждому
    процессу отправлять место в матрице - ответе, куда нужно будет вставить
    результат умножения.
    
    ---------
    
    Co-authored-by: Egor1dzeN-learn <egormorozov123450@gmail.com>

---
+++ tasks/mpi/morozov_e_mult_sparse_matrix/include/ops_mpi.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace morozov_e_mult_sparse_matrix {
+template <typename T>
+T scalMultOfVectors(const std::vector<T> &vA, const std::vector<T> &vB);
+std::pair<std::vector<std::vector<double>>, std::vector<std::vector<double>>> convertToBasicMatrixs(
+    const std::vector<double> &dA, const std::vector<int> &row_indA, const std::vector<int> &col_indA,
+    const std::vector<double> &dB, const std::vector<int> &row_indB, const std::vector<int> &col_indB, int rowsA,
+    int columnsA, int rowsB, int columnsB);
+void convertToCCS(const std::vector<std::vector<double>> &matrix, std::vector<double> &values,
+                  std::vector<int> &row_indices, std::vector<int> &col_pointers);
+void fillData(std::shared_ptr<ppc::core::TaskData> &taskData, int rowsA, int columnsA, int rowsB, int columnsB,
+              std::vector<double> &dA, std::vector<int> &row_indA, std::vector<int> &col_indA, std::vector<double> &dB,
+              std::vector<int> &row_indB, std::vector<int> &col_indB, std::vector<std::vector<double>> &out);
+class TestTaskSequential : public ppc::core::Task {
+ public:
+  explicit TestTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<std::vector<double>> ans;
+  std::vector<double> dA, dB;
+  std::vector<int> row_indA, row_indB, col_indA, col_indB;
+  int rowsA, rowsB, columnsA, columnsB, dA_size, dB_size, row_indA_size, row_indB_size, col_indA_size, col_indB_size;
+};
+template <typename T>
+void printMatrix(std::vector<std::vector<T>> m);
+template <typename T>
+void printVector(std::vector<T> v);
+
+class TestMPITaskParallel : public ppc::core::Task {
+ public:
+  explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<std::vector<double>> ans;
+  std::vector<double> dA, dB, local_input_A, local_input_B;
+  std::vector<int> row_indA, row_indB, col_indA, col_indB, local_input_;
+  int rowsA, rowsB, columnsA, columnsB, dA_size, dB_size, row_indA_size, row_indB_size, col_indA_size, col_indB_size;
+  boost::mpi::communicator world;
+};
+}  // namespace morozov_e_mult_sparse_matrix
